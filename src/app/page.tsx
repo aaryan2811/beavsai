d41e163e39eca5a887614d62eea89aa267cd4bbd
Thomas <nyumat18@gmail.com>
2024-10-26 17:32:22 -0700

    ui: fix hdration errors with next 15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import SignIn from "@/components/sign-in";
+import { ThemeToggle } from "@/components/theme-toggle";
 import { auth } from "@/lib/auth";
 
 export default async function Home() {
@@ -6,6 +7,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <SignIn />
+      <ThemeToggle />
       {session?.user ? (
         <div>
           <p>Currently signed in as {session.user.email}</p>
